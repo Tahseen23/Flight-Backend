450fbd3a2ad0192a967bfca1c8056704a516945d
Tahseen Kausar <tahseenkausar24@gmail.com>
2025-01-28 00:07:11 +0530

    Added basic functionalitize

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const {PORT}=require('./config')
+const express=require('express')
+const apiRoutes=require('./routes/index.js')
+const {serverConfig}=require('./config')
+
+const app=express()
+
+app.use('/api',apiRoutes)
+
+app.listen(serverConfig.PORT,()=>{
+  console.log(`Server is running at ${serverConfig.PORT}`)
+})
